ee63f3ea86a42c0408762859f1ead8d2f8c84c12
devnos <iamnoelsto@gmail.com>
2022-07-24 20:30:56 +0200

    Add content to about page

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import routeMap from "../../constants/routeMap";
 
 const LogoText = styled.div`
   font-family: "Manrope", sans-serif;
@@ -9,12 +10,12 @@ const Logo = () => {
   return (
     <div className="logo-wrap" data-hover-hint="logo">
       <LogoText className="logo e2e-site-logo-text logo-image has-rollover">
-        <a href="/work" className="image-normal image-link">
+        <a href={routeMap["HOME"]} className="image-normal image-link">
           NoelSantoTomas
         </a>
       </LogoText>
       <div className="logo-secondary logo-secondary-text">
-        <a href="/work" className="preserve-whitespace">
+        <a href={routeMap["HOME"]} className="preserve-whitespace">
           UX Designer
         </a>
       </div>
