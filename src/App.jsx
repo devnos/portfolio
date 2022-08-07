5b447d1bf6c2fa4e0b166545f2bed17e7ef68843
Diogo Redin <me@diogoredin.com>
2022-08-07 11:55:45 +0200

    Change default page

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import GiveBack from "./pages/projects/GiveBack";
-import SafeOnline from "./pages/projects/SafeOnline";
-import BBC from "./pages/projects/BBC";
-import MySkin from "./pages/projects/MySkin";
-import Cueshty from "./pages/projects/Cueshty";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/giveback" element={<GiveBack />} />
-        <Route path="/safeonline" element={<SafeOnline />} />
-        <Route path="/bbc" element={<BBC />} />
-        <Route path="/myskin" element={<MySkin />} />
-        <Route path="/cueshty" element={<Cueshty />} />
       </Routes>
     </Router>
   );
