a948500b9cf2896edd696a3a5010934b101c3ee7
devnos <iamnoelsto@gmail.com>
2022-08-11 11:54:33 +0200

    Add correct project images & details

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -15,7 +15,7 @@ const Projects = styled.div`
   }
 `;
 
-const Project = styled.div`
+const Project = styled.a`
   position: relative;
   width: calc(100% / 3);
 
@@ -26,12 +26,23 @@ const Project = styled.div`
   .details {
     visibility: hidden;
     opacity: 0;
+    padding: 1rem 2rem;
+    color: #fff;
     transition: visibility 0.3s linear, opacity 0.3s linear;
+  }
+
+  .details > h1 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
   }
 
   &:hover > .details {
     visibility: visible;
     opacity: 1;
+  }
+
+  :visited {
+    color: inherit;
   }
 `;
 
@@ -42,11 +53,19 @@ const ProjectCover = styled.img`
 
 const ProjectDetails = styled.div`
   position: absolute;
-  background-color: rgba(30, 149, 179, 0.7);
+  background-color: rgba(30, 149, 179, 0.75);
   top: 0;
   left: 0;
   height: 100%;
   width: 100%;
+`;
+
+const Date = styled.p`
+  padding-bottom: 0px;
+`;
+
+const Tags = styled.p`
+  font-weight: bold;
 `;
 
 const Home = () => {
@@ -56,33 +75,46 @@ const Home = () => {
       <Hero />
       <Projects>
         <Project href={routeMap["GIVEBACK"]}>
-          <ProjectCover src="/images/covers/giveback.png" alt="GiveBack" />
+          <ProjectCover src="images/covers/GiveBack_Cover.png" alt="GiveBack" />
           <ProjectDetails className="details">
             <h1>GiveBack</h1>
+            <Date>2021</Date>
+            <Tags>UX, UI</Tags>
           </ProjectDetails>
         </Project>
         <Project href={routeMap["SAFEONLINE"]}>
-          <ProjectCover src="/images/covers/giveback.png" alt="GiveBack" />
+          <ProjectCover
+            src="/images/covers/SafeOnline_Cover.png"
+            alt="GiveBack"
+          />
           <ProjectDetails className="details">
             <h1>SafeOnline</h1>
+            <Date>2021</Date>
+            <Tags>Branding, UX, UI</Tags>
           </ProjectDetails>
         </Project>
         <Project href={routeMap["BBC"]}>
-          <ProjectCover src="/images/covers/giveback.png" alt="GiveBack" />
+          <ProjectCover src="/images/covers/BBC_Cover.png" alt="GiveBack" />
           <ProjectDetails className="details">
             <h1>BBC</h1>
+            <Date>2020</Date>
+            <Tags>UX, UI</Tags>
           </ProjectDetails>
         </Project>
         <Project href={routeMap["MYSKIN"]}>
-          <ProjectCover src="/images/covers/giveback.png" alt="GiveBack" />
+          <ProjectCover src="/images/covers/MySkin_Cover.jpg" alt="GiveBack" />
           <ProjectDetails className="details">
             <h1>MySkin</h1>
+            <Date>2021</Date>
+            <Tags>UX, UI, Branding, Social Media</Tags>
           </ProjectDetails>
         </Project>
         <Project href={routeMap["CUESHTY"]}>
-          <ProjectCover src="/images/covers/giveback.png" alt="GiveBack" />
+          <ProjectCover src="/images/covers/Cueshty_Cover.png" alt="GiveBack" />
           <ProjectDetails className="details">
             <h1>Cueshty</h1>
+            <Date>2021</Date>
+            <Tags>Branding, Motion Graphics</Tags>
           </ProjectDetails>
         </Project>
       </Projects>
